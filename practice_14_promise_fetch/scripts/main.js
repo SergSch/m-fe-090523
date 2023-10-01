e02ff09cb3f213d00b2650fdf47ab1f6d9efb450
Davy <davyd.dabahian@cheetahdigital.com>
2023-10-01 16:03:57 +0300

    материалы 15-го занятия: fetch, Promise.all

--- practice_14_promise_fetch/scripts/main.js
+++ practice_14_promise_fetch/scripts/main.js
@@ -1,1 +1,76 @@
 // Здесь пока ничего нет.
+
+/*
+1. Пишем сетевой запрос. Наша задача - обратиться к "бэку" по адресу `https://fakestoreapi.com/products/categories` GET-запросом, и вывести в консоль результат в "удобоваримом" виде (результат нам возвращается в виде JSON). Этот запрос нам вернет массив с категориями товаров.
+*/
+
+
+/*
+2. Усложняем задачу. Нужно написать функцию `renderSelect`. Она должна:
+ Добавляем в разметку ПЕРЕД блоком `.productsContainer` компонент `select` (выпадающее меню). Нужно заполнить этот `select` опциями (`<option value='value'>option_text</option>`), которые нам пришли с "бэка" в предыдущей задаче. То есть, пройтись по массиву результатов, для каждого создать элемент `option`, и добавить его в наш `select`. В качестве `value` используем значение, в качестве `option_text` используем то же, но с большой буквы (т.е. надо сделать первую букву значения заглавной).
+*/
+
+const categoriesSelect = document.querySelector('#categoriesSelect');
+
+const renderSelect = () => {
+    // запросить данные категорий
+    fetch('https://fakestoreapi.com/products/categories')
+        .then( resp => resp.json())
+        .then( data => {
+            console.log('Response data: ', data);
+            // отрендерить их в селекте
+            data.forEach( elem => {
+                const optionEl = document.createElement('option');
+                optionEl.value = elem;
+                optionEl.textContent = elem.charAt(0).toUpperCase() + elem.slice(1);
+                categoriesSelect.append(optionEl);
+            });
+        });
+}
+
+renderSelect();
+
+/*
+3. Пишем функцию `getProducts`. В качестве аргумента, она принимает категорию товаров. Она должна сделать запрос по адресу `https://fakestoreapi.com/products/category/CATEGORY`. В ответ нам придет список товаров этой категории. Наша задача - отрендерить их в списке товаров (`div.productsContainer`). ВАЖНО! Список товаров перед рендерингом надо очистить! Товары рендерим в виде карточек, класс - `productCard`.
+*/
+
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+
+const productsContainer = document.querySelector('#productsContainer');
+function getProducts(category) {
+
+    fetch(`https://fakestoreapi.com/products/category/${category}`)
+        .then( resp => resp.json())
+        .then( data => {
+            data.forEach( product => {
+                const productCard = document.createElement('div');
+                productCard.classList.add('productCard');
+                const title = document.createElement('h3');
+                title.textContent = product.title;
+                const price = document.createElement('p');
+                price.textContent = product.price;
+                const description = document.createElement('p');
+                description.textContent = product.description;
+
+                productCard.append(title, price, description);
+                productsContainer.append(productCard);
+            });
+            loader.style.display = 'none';
+        });
+
+}
+
+/*
+5. Добавляем обработчик события `change` на наш `select`. При событии, нам нужно вызывать функцию из п.12 с соответствующей категорией - т.е. мы рисуем список товаров выбранной категории.
+*/
+
+categoriesSelect.addEventListener('change', (event) => {
+    productsContainer.innerHTML = '';
+    loader.style.display = 'block';
+    getProducts(event.target.value);
+});
+
+/**
+ * 6. Добавляем "иконку загрузки" на нашу страницу. Добавляем перед нашим `select` контейнер (`div.loader`) с текстом "Подождите, идет загрузка...". По умолчанию он скрыт (`display: none`). Нам нужно показывать этот блок при начале запроса списка товаров, и скрывать, когда список товаров успешно загружен.
+ */
---
+++ practice_15_promise_fetch_practice/scripts/main.js
@@ -0,0 +1,148 @@
+// Здесь пока ничего нет.
+
+const categoriesSelect = document.querySelector('#categoriesSelect');
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+const productsContainer = document.querySelector('#productsContainer');
+
+/**
+ * Функция запроса списка товаров
+ * @param {*} category Категория
+ * @returns ПРОМИС, который будет зарезолвлен НТТР-ответом
+ */
+const requestProducts = (category) => {
+    const productsUrl = `https://fakestoreapi.com/products/` + (!!category ? `category/${category}` : '');
+    return fetch(productsUrl);
+};
+
+/**
+ * Функция запроса списка категорий
+ * @returns ПРОМИС, который будет зарезолвлен НТТР-ответов
+ */
+const requestCategories = () => {
+    return fetch('https://fakestoreapi.com/products/categories');
+}
+
+/**
+ * Функция, которая рендерит категории
+ * @param {*} categoriesArray Массив с категориями
+ */
+const renderCategories = (categoriesArray) => {
+    categoriesArray.forEach( elem => {
+        const optionEl = document.createElement('option');
+        optionEl.value = elem;
+        optionEl.textContent = elem.charAt(0).toUpperCase() + elem.slice(1);
+        categoriesSelect.append(optionEl);
+    });
+}
+
+/**
+ * Функция которая рендеритт карточки товаров
+ * @param {*} productsArray Массив с данными товаров
+ */
+const renderProducts = (productsArray) => {
+    // Очищаем контейнер товаров
+    productsContainer.innerHTML = '';
+    // Отрисовываем карточки товаров
+    productsArray.forEach( product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('productCard');
+        const title = document.createElement('h3');
+        title.textContent = product.title;
+        const price = document.createElement('p');
+        price.textContent = product.price;
+        const description = document.createElement('p');
+        description.textContent = product.description;
+        const dropProductBtn = document.createElement('a');
+        dropProductBtn.classList.add('dropProductBtn');
+        dropProductBtn.innerText = 'Drop';
+        productCard.append(title, price, description, dropProductBtn);
+        productsContainer.append(productCard);
+    });
+}
+
+// Блок, который отработает при запуске скрипта.
+const init = () => {
+    loader.style.display = 'block';
+    const categoriesPromise = requestCategories();
+    const productsPromise = requestProducts();
+    Promise.all([categoriesPromise, productsPromise])
+        .then( (results) => {
+            return Promise.all([results[0].json(), results[1].json()]);
+        })
+        .then( results => {
+            renderCategories(results[0]);
+            renderProducts(results[1]);
+            loader.style.display = 'none';
+        });
+
+    categoriesSelect.addEventListener('change', (event) => {
+        loader.style.display = 'block';
+        requestProducts(event.target.value)
+            .then(resp => resp.json())
+            .then(products => {
+                renderProducts(products);
+                loader.style.display = 'none';
+            });
+    });
+}
+
+init();
+
+// Для задачи №2
+const task2Demo = () => {
+    const firstPromise = new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            console.log('First promise finished!')
+            resolve(5)
+        }, 2000);
+    });
+    const secondPromise = new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            console.log('Second promise finished!')
+            resolve(7)
+        }, 3000);
+    });
+    // Нужно получить сумму того, чем зарезолвлены наши промисы
+}
+
+// task2Demo();
+
+// Для задачи №4
+const task4Demo = () => {
+    const firstPromise = new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            console.log('First promise finished!')
+            resolve(5)
+        }, 2000);
+    });
+    const secondPromise = new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            const rand = Math.random();
+            if (rand < 0.5) {
+                console.log('Second promise rejected!')
+                reject(7);
+            }
+            else {
+                console.log('Second promise finished!')
+                resolve(7);
+            }
+        }, 3000);
+    });
+}
+
+// task4Demo();
+
+// Работа с модальным окном формы добавления нового товара
+const addProductBtn = document.querySelector('#addProductBtn');
+const productModal = document.querySelector('#modalContainer');
+
+addProductBtn.addEventListener('click', (event) => {
+    productModal.style.display = 'block';
+});
+
+productModal.addEventListener('click', (event) => {
+    if (event.target === productModal) {
+        productModal.style.display = 'none';
+    }
+})
