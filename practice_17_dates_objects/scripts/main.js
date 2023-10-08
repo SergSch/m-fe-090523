b782745f7c3eb556bb819a397e0015dfb54f79f0
Davy <davyd.dabahian@cheetahdigital.com>
2023-10-08 18:09:34 +0300

    Материалы 17-го занятия: даты, деструктуризация, spread, rest

---
+++ practice_17_dates_objects/scripts/main.js
@@ -0,0 +1,51 @@
+// код пишем здесь.
+
+const car1 = {
+    model: 'Lada',
+    type: 'sedan',
+    color: 'baklazhan',
+    manufactured: 1989
+}
+
+const car2 = {
+    model: 'Lada',
+    type: 'sedan',
+    color: 'baklazhan',
+    manufactured: 1989
+}
+
+const car3 = car1;
+
+const dwarf = {
+    name: 'Karst',
+    level: 12,
+    fraction: 'Dwarwes',
+    skills: {
+        magic: 0,
+        melee: 11,
+        archery: 0,
+    }
+}
+
+const baseDwarfWeapons = [
+    {
+        type: 'melee',
+        name: 'Sword',
+    },
+    {
+        type: 'melee',
+        name: 'Axe',
+    },
+    {
+        type: 'melee',
+        name: 'Spear',
+    },
+    {
+        type: 'melee',
+        name: 'Flail',
+    },
+    {
+        type: 'archery',
+        name: 'crossbow',
+    },
+];
