b9a4333291a79255b613691e790dad42f6bc2cff
Davy <davyd.dabahian@cheetahdigital.com>
2023-12-13 13:40:45 +0200

    материалы 36-го занятия - react router

--- practice_36_react_router/src/components/greeting/Greeting.jsx
+++ practice_36_react_router/src/components/greeting/Greeting.jsx
@@ -1,24 +1,8 @@
-import { useEffect } from "react"
-import { useState } from "react"
 
 export default function Greeting() {
 
-  const [time, setTime] = useState(new Date().toLocaleString());
-
-  useEffect( () => {
-
-    const intervalId = setInterval( () => {
-      setTime(new Date().toLocaleString());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    }
-  }, []);
-
   return (
     <div>
-      <p>{time}</p>
       <p>{`Hello, stranger!`}</p>
     </div>
   )
