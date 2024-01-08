14d574765a72a9aeec7597fbc8832e184a2c7eaa
Davy <davyd.dabahian@cheetahdigital.com>
2024-01-08 15:28:57 +0200

    results

--- practice_39_react_recovery/src/App.js
+++ practice_39_react_recovery/src/App.js
@@ -6,31 +6,36 @@ import SimpleComponent from './components/simpleComponent/SimpleComponent.jsx';
 import { Routes, Route, Link } from 'react-router-dom';
 import Greeting from './components/greeting/Greeting.jsx';
 import ProductList from './components/productList/ProductList.jsx';
+import ProductDetail from './components/productDetail/ProductDetail.jsx';
+import { CartContext, CartContextProvider } from './context/CartContext.js';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <p>Hello World from React! {new Date().toLocaleString()} </p>
+    <CartContextProvider>
+      <div className="App">
+        <p>Hello World from React! {new Date().toLocaleString()} </p>
 
-        {/*
-        6. Теперь создаем линки на эти маршруты. В компоненте `App`, добавить ссылки с текстами "Авторизация", "Наши товары", которые ведут на страницу авторизации и страницу со списком товаров соответственно.
-         */}
-        <div>
-          <Link to='/login'>Login Page</Link>
-          <Link to='/products'>Products Page</Link>
-        </div>
+          {/*
+          6. Теперь создаем линки на эти маршруты. В компоненте `App`, добавить ссылки с текстами "Авторизация", "Наши товары", которые ведут на страницу авторизации и страницу со списком товаров соответственно.
+          */}
+          <div>
+            <Link to='/login'>Login Page</Link>
+            <Link to='/products'>Products Page</Link>
+          </div>
 
-        <Routes>
-          <Route path='/' element={<Greeting /> }/>
-          <Route path='/login' element={<RegisterForm />} />
-          <Route path='/products' element={<ProductList /> } />
-          <Route path='*' element={<h3>Sorry, invalid page name!</h3>}/>
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Greeting /> }/>
+            <Route path='/login' element={<RegisterForm />} />
+            <Route path='/products' element={<ProductList /> } />
+            <Route path='/products/:id' element={<ProductDetail /> } />
+            <Route path='*' element={<h3>Sorry, invalid page name!</h3>}/>
+          </Routes>
 
-      <SimpleComponent />
-    </div>
+        <SimpleComponent />
+      </div>
+      </CartContextProvider>
   );
 }
 
--- practice_39_react_recovery/src/components/productList/ProductList.jsx
+++ practice_39_react_recovery/src/components/productList/ProductList.jsx
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext';
 
 export default function ProductList() {
 
   const [products, setProducts] = useState([]);
+
+  const cartContext = useContext(CartContext);
+  console.log('cartContext', cartContext)
 
   useEffect( () => {
     fetch('https://dummyjson.com/products')
       .then( resp => resp.json())
       .then( data => {
         setProducts(data.products);
+        cartContext.addToCart(1);
        } );
   }, []);
 
@@ -17,7 +23,7 @@ export default function ProductList() {
       { products.map( elem => {
         return (
           <div key={elem.id}>
-            <h4>{elem.title}</h4>
+            <Link to={`${elem.id}`}><h4>{elem.title}</h4></Link>
             <p>{elem.description}</p>
           </div>
         );
